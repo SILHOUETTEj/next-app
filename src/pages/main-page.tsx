5cf28103c8e905a5a116e8c8207c21d7305c9a2a
Danya <danil-romanyuk-2000@mail.ru>
2024-11-18 13:24:29 +0300

    Init

---
+++ src/pages/main-page.tsx
@@ -0,0 +1,9 @@
+const MainPage = () => {
+  return (
+    <div className="bg-gray-100">
+        <h1>Главная страница</h1>
+    </div>
+  )
+}
+
+export default MainPage
---
+++ src/pages/second-page.tsx
@@ -0,0 +1,7 @@
+const SecondPage = () => {
+  return (
+    <h1>Вторая страница</h1>
+  )
+}
+
+export default SecondPage
